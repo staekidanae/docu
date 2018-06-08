751c193e4518dbf99a8429b16a177d8cf4563458
staekidanae <staekidanae@gmail.com>
2018-06-08 16:39:20 +0900

    choices, verbose_name, help_text 실습

---
+++ app/models/fields/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FieldsConfig(AppConfig):
+    name = 'models.fields'
+    # models 안의 패키지기에 models.fields
---
+++ app/models/fields/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Person(models.Model):
+    SHIRT_SIZE = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
+
+    name = models.CharField(max_length=60)
+    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZE)
+
