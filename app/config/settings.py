f5441d21b1a5ba2905cd55008a9c63d5d060a8f5
staekidanae <staekidanae@gmail.com>
2018-06-18 12:28:58 +0900

    단순 ManyToMany관계구성

--- app/config/settings.py
+++ app/config/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'models.fields.apps.FieldsConfig',
     'models.foreignkey.apps.ForeignkeyConfig',
+    'models.manytomany.apps.ManytomanyConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
